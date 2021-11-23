6aa5b282ea660920a26557d5e42c80f7697b58e6
= <=>
2021-11-23 04:43:34 +0100

    added routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DragDropModule } from '@angular/cdk/drag-drop';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
@@ -10,16 +11,27 @@ import { MatListModule } from '@angular/material/list';
 import { KanbanComponent } from './kanban/kanban.component';
 import { NotesComponent } from './notes/notes.component';
 import { PomodoroComponent } from './pomodoro/pomodoro.component';
+import { OverviewComponent } from './overview/overview.component';
+
+const routes: Routes = [
+  { path: 'overview', component: OverviewComponent },
+  { path: 'notes', component: NotesComponent },
+  { path: 'kanban', component: KanbanComponent },
+  { path: 'pomodoro', component: PomodoroComponent },
+  { path: '', redirectTo: 'overview', pathMatch: 'full' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     KanbanComponent,
     NotesComponent,
-    PomodoroComponent
+    PomodoroComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes),
     DragDropModule,
     BrowserAnimationsModule,
     MatCardModule,
